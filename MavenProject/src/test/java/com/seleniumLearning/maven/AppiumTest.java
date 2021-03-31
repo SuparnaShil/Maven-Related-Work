4600af7b88ec96f65dd606f64771d7bb53199ac6
Suparna Shil <50476554+SuparnaShil@users.noreply.github.com>
2021-03-31 13:57:20 +0600

    New files

---
+++ MavenProject/src/test/java/com/seleniumLearning/maven/AppiumTest.java
@@ -0,0 +1,5 @@
+package SeleniumMavenProjectJavaNew;
+
+public class AppiumTest {
+  
+}
---
+++ MavenProject/src/test/java/com/seleniumLearning/maven/SeleniumTest.java
@@ -0,0 +1,27 @@
+package SeleniumMavenProjectJavaNew;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	
+	@Test 
+	public void elementsUI ()
+	{
+		System.out.println("elementsUI");
+	}
+	
+	
+	@Test 
+	public void LoginTest ()
+	{
+		System.out.println("LoginTest");
+	}
+	
+}
